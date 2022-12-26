dd46b3b2b96e3413e0f03ca173234b46c1d99dd6
Paulo Sergio <pauloserg861@gmail.com>
2022-12-26 15:19:33 -0300

    criando menu de links

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import TemplateDefault from "./components/Template/Default";
+
+import menuList from "./constants/menu-list";
 import avatar from "../src/assets/img/avatar-paulo.png";
 import styles from "./styles/App.module.css";
 
 const App = () => {
 	return (
-		<TemplateDefault>
-			<main className={styles.content}>
-				<div className={styles.profile}>
-					<div className={styles.avatar}>
-						<img src={avatar} alt="avatar Paulo" />
+		<>
+			<TemplateDefault>
+				<main className={styles.content}>
+					<div className={styles.profile}>
+						<div className={styles.avatar}>
+							<img src={avatar} alt="avatar Paulo" />
+						</div>
+						<h1>@paulo_devmais</h1>
 					</div>
-					<h1>@paulo_devmais</h1>
-				</div>
-				<div>
-					<ul>
-						<li>lista de links</li>
-					</ul>
-				</div>
-			</main>
-		</TemplateDefault>
+					<div className={styles.links_box}>
+						<ul>
+							{menuList.map((link) => (
+								<li className={styles.listItem} key={link.id}>
+									<a href={link.path}>
+										<h3>{link.name}</h3>
+										<p>{link.description}</p>
+									</a>
+								</li>
+							))}
+						</ul>
+					</div>
+					<footer className={styles.footer}>
+						Este aplicativo foi criado por Dev++
+					</footer>
+				</main>
+			</TemplateDefault>
+		</>
 	);
 };
 
---
+++ src/constants/menu-list.ts
@@ -0,0 +1,20 @@
+export default [
+	{
+		id: 1,
+		path: "https://paulodevmais.netlify.app/portfolio",
+		name: "Portfólio",
+		description: "Conheça meus projetos de desenvolvimento web.",
+	},
+	{
+		id: 2,
+		path: "https://www.instagram.com/dev.mais/",
+		name: "Dev++",
+		description: "Siga-me no instagram e vamos compartilhar conhecimento.",
+	},
+	{
+		id: 3,
+		path: "https://www.instagram.com/hpmarketingvisual/",
+		name: "Hp Design & Web",
+		description: "Conheça meus trabalhos como design de criação.",
+	},
+];
